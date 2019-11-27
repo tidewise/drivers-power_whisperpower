d405e41130f52c4a45dcb45b785e309454cc16f9
Sylvain <sylvain.joyeux@tidewise.io>
2019-11-27 20:06:54 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace power_whisperpower
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace power_whisperpower
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <power_whisperpower/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    power_whisperpower::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
