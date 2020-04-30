74429c273bbf6de7b277f14619eab3c8319aee46
Sylvain <sylvain.joyeux@tidewise.io>
2020-04-30 18:46:36 -0300

    document DCPowerCube full update tracking functionality

--- src/DCPowerCube.hpp
+++ src/DCPowerCube.hpp
@@ -5,6 +5,21 @@
 #include <power_whisperpower/DCPowerCubeStatus.hpp>
 
 namespace power_whisperpower {
+    /** Driver for the WhisperPower DC Cube
+     *
+     * Since DC power cubes are connected through a CAN bus, this class only
+     * provides interpretation and generation of CAN messages. Use one CAN
+     * driver from Rock's drivers/canbus (or elsewhere) to actually transmit/
+     * receive messages from the bus
+     *
+     * The main functionality of this driver is to interpret the suite of CAN
+     * messages that form the DC cube's status update. The pattern is to call
+     * \c resetFullUpdate and check \c hasFullUpdate after each processed
+     * CAN message. When \c hasFullUpdate is true, you know you the cube
+     * status has been fully updated and can call \c getStatus.
+     *
+     * Then call \c resetFullUpdate to start a new cycle
+     */
     class DCPowerCube : public Device {
         typedef DCPowerCubeStatus Status;
 
@@ -18,9 +33,19 @@ namespace power_whisperpower {
 
     public:
         DCPowerCube(uint8_t device_id);
+
+        /** Return the last known DC cube status */
         Status getStatus() const;
 
+        /** Check if a full update to the smart shunt status has been received
+         *
+         * True if the whole status structure has been updated at least once
+         * since the last call to \c resetFullUpdate
+         */
         bool hasFullUpdate() const;
+
+        /** Reset \c hasFullUpdate to wait for a new full update
+         */
         void resetFullUpdate();
     };
 }
