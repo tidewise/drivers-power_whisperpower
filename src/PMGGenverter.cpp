1ad2d8e49152b80f7099b4754b00fe1322a2bedc
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2025-03-24 13:55:25 -0300

    chore: fix nonsensical offsets and review according to docs
    The whole CAN-to-status conversion was totally broken. Fix based on
    documentation

--- src/PMGGenverter.cpp
+++ src/PMGGenverter.cpp
@@ -12,49 +12,49 @@ void PMGGenverter::process(canbus::Message const& msg)
     switch (msg.can_id) {
         case 0x200:
             m_status.time = base::Time::now();
-            m_status.ac_voltage = static_cast<float>(fromBigEndian<uint16_t>(value)) / 10;
+            m_status.ac_voltage = static_cast<float>(fromBigEndian<uint16_t>(msg.data)) / 10;
             m_status.ac_current =
-                static_cast<float>(fromBigEndian<uint16_t>(value + 2)) / 10;
+                static_cast<float>(fromBigEndian<uint16_t>(msg.data + 2)) / 10;
             m_status.engine_angular_speed =
-                static_cast<float>(fromBigEndian<uint16_t>(value + 8)) * 60 * 2 * M_PI;
+                static_cast<float>(fromBigEndian<uint16_t>(msg.data + 4)) * 60 * 2 * M_PI;
             m_status.inverter_temperature = base::Temperature::fromCelsius(
-                fromBigEndian<uint16_t>(value + 32) / 10.0);
+                fromBigEndian<uint16_t>(msg.data + 6) / 10.0);
             break;
         case 0x201:
-            m_status.status = value[0];
-            m_status.inverter_alarm = value[1];
-            m_status.inverter_warning = value[2];
-            m_status.engine_alarm = value[3];
+            m_status.status = fromBigEndian<uint16_t>(msg.data + 0);
+            m_status.inverter_alarm = fromBigEndian<uint16_t>(msg.data + 2);
+            m_status.inverter_warning = fromBigEndian<uint16_t>(msg.data + 4);
+            m_status.engine_alarm = fromBigEndian<uint16_t>(msg.data + 6);
             break;
         case 0x202:
-            m_status.stepper = static_cast<float>(fromBigEndian<uint16_t>(value));
+            m_status.stepper = static_cast<float>(fromBigEndian<uint16_t>(msg.data));
             m_status.oil_temperature =
-                base::Temperature::fromCelsius(fromBigEndian<uint16_t>(value + 2) / 10.0);
+                base::Temperature::fromCelsius(fromBigEndian<uint16_t>(msg.data + 2) / 10.0);
             m_status.delta_dc_bus =
-                static_cast<float>(fromBigEndian<uint16_t>(value + 8));
-            m_status.PWM_scale = static_cast<float>(fromBigEndian<uint16_t>(value + 32));
+                static_cast<float>(fromBigEndian<uint16_t>(msg.data + 4));
+            m_status.PWM_scale = static_cast<float>(fromBigEndian<uint16_t>(msg.data + 6));
             break;
         case 0x203:
-            m_status.TEST_ramp = static_cast<float>(value[7]);
+            m_status.TEST_ramp = static_cast<float>(msg.data[7]);
             m_has_full_update = !m_status.time.isNull();
             break;
         case 0x204:
-            m_status.inverter_model = fromBigEndian<std::string>(value);
-            m_status.firmware_version = value[2];
-            m_status.firmware_subversion = value[3];
-            m_status.hardware_version = value[4];
-            m_status.hardware_subversion = value[5];
-            m_status.inverter_serial_number = fromBigEndian<std::string>(value + 64);
+            m_status.inverter_model = fromBigEndian<uint16_t>(msg.data);
+            m_status.firmware_version = msg.data[2];
+            m_status.firmware_subversion = msg.data[3];
+            m_status.hardware_version = msg.data[4];
+            m_status.hardware_subversion = msg.data[5];
+            m_status.inverter_serial_number = fromBigEndian<uint16_t>(msg.data + 6);
             break;
         case 0x205:
             m_status.total_hour_work =
-                base::Time::fromSeconds(fromBigEndian<uint16_t>(value) * 3600);
+                base::Time::fromSeconds(fromBigEndian<uint16_t>(msg.data) * 3600);
             m_status.total_minute_work =
-                base::Time::fromSeconds(fromBigEndian<uint16_t>(value + 2) * 60);
+                base::Time::fromSeconds(fromBigEndian<uint16_t>(msg.data + 2) * 60);
             m_status.maintenance_hour_work =
-                base::Time::fromSeconds(fromBigEndian<uint16_t>(value + 8) * 3600);
+                base::Time::fromSeconds(fromBigEndian<uint16_t>(msg.data + 4) * 3600);
             m_status.maintenance_minute_work =
-                base::Time::fromSeconds(fromBigEndian<uint16_t>(value + 32) * 60);
+                base::Time::fromSeconds(fromBigEndian<uint16_t>(msg.data + 6) * 60);
             break;
     }
 }
